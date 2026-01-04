db75984d6659142a7e4eac8e891aa8dd09a9b201
hanish.goyal <73vjkxna3bv74pkbjwtxxqq7sxw5zcuujbnwpchoiroigy5yocfq>
2026-01-04 14:18:04 +0530

    changed logic for update item quality using max and min functions

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -9,32 +9,34 @@ class GildedRose(object):
         for item in self.items:
             if item.name == "Aged Brie":
                 item.sell_in = item.sell_in - 1
-                if item.quality < 50:
-                    item.quality = item.quality + 1
-                if item.sell_in < 0 and item.quality < 50:
-                    item.quality = item.quality + 1
+                if item.sell_in < 0:
+                    item.quality = min(item.quality + 1 , 50)
+                item.quality = min(item.quality + 1, 50)
+
             elif item.name == "Sulfuras, Hand of Ragnaros":
                 pass
+
             elif item.name == "Backstage passes to a TAFKAL80ETC concert":
                 item.sell_in = item.sell_in - 1
-                if item.quality < 50:
-                    item.quality = item.quality + 1
-                if item.sell_in < 10 and item.quality < 50:
-                    item.quality = item.quality + 1
-                if item.sell_in < 5 and item.quality < 50:
-                    item.quality = item.quality + 1
                 if item.sell_in < 0:
                     item.quality = 0
+                elif item.sell_in < 5:
+                    item.quality = min(item.quality + 3,  50)
+                elif item.sell_in < 10:
+                    item.quality = min(item.quality + 2,  50)
+                else:
+                    item.quality = min(item.quality + 1, 50)
+
             elif item.name == "Conjured Mana Cake":
                 item.sell_in = item.sell_in - 1
                 item.quality = max(item.quality -2 , 0)
                 if item.sell_in < 0:
                     item.quality = max(item.quality -2 , 0)
+
             else:
                 item.sell_in = item.sell_in - 1
-                if item.quality > 0:
-                    item.quality = item.quality - 1
-                if item.sell_in < 0 < item.quality:
-                    item.quality = item.quality - 1
+                if item.sell_in < 0:
+                    item.quality = max(item.quality - 1, 0)
+                item.quality = max(item.quality - 1, 0)
 
 
