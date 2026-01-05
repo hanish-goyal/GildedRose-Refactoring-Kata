a31b2f6f1fb52ae29972d954a4f7a803c57a7414
hanish.goyal <73vjkxna3bv74pkbjwtxxqq7sxw5zcuujbnwpchoiroigy5yocfq>
2026-01-05 10:52:14 +0530

    added a static method in item class to update the quality

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
+from item import Item
+
 
 class GildedRose(object):
 
@@ -6,37 +8,4 @@ class GildedRose(object):
         self.items = items
 
     def update_quality(self):
-        for item in self.items:
-            if item.name == "Aged Brie":
-                item.sell_in = item.sell_in - 1
-                if item.sell_in < 0:
-                    item.quality = min(item.quality + 1 , 50)
-                item.quality = min(item.quality + 1, 50)
-
-            elif item.name == "Sulfuras, Hand of Ragnaros":
-                pass
-
-            elif item.name == "Backstage passes to a TAFKAL80ETC concert":
-                item.sell_in = item.sell_in - 1
-                if item.sell_in < 0:
-                    item.quality = 0
-                elif item.sell_in < 5:
-                    item.quality = min(item.quality + 3,  50)
-                elif item.sell_in < 10:
-                    item.quality = min(item.quality + 2,  50)
-                else:
-                    item.quality = min(item.quality + 1, 50)
-
-            elif item.name == "Conjured Mana Cake":
-                item.sell_in = item.sell_in - 1
-                item.quality = max(item.quality -2 , 0)
-                if item.sell_in < 0:
-                    item.quality = max(item.quality -2 , 0)
-
-            else:
-                item.sell_in = item.sell_in - 1
-                if item.sell_in < 0:
-                    item.quality = max(item.quality - 1, 0)
-                item.quality = max(item.quality - 1, 0)
-
-
+        Item.update_quality(self.items)
--- python/item.py
+++ python/item.py
@@ -6,3 +6,38 @@ class Item:
 
     def __repr__(self):
         return "%s, %s, %s" % (self.name, self.sell_in, self.quality)
+
+    @staticmethod
+    def update_quality(items):
+        for item in items:
+            if item.name == "Aged Brie":
+                item.sell_in = item.sell_in - 1
+                if item.sell_in < 0:
+                    item.quality = min(item.quality + 1 , 50)
+                item.quality = min(item.quality + 1, 50)
+
+            elif item.name == "Sulfuras, Hand of Ragnaros":
+                pass
+
+            elif item.name == "Backstage passes to a TAFKAL80ETC concert":
+                item.sell_in = item.sell_in - 1
+                if item.sell_in < 0:
+                    item.quality = 0
+                elif item.sell_in < 5:
+                    item.quality = min(item.quality + 3,  50)
+                elif item.sell_in < 10:
+                    item.quality = min(item.quality + 2,  50)
+                else:
+                    item.quality = min(item.quality + 1, 50)
+
+            elif item.name == "Conjured Mana Cake":
+                item.sell_in = item.sell_in - 1
+                item.quality = max(item.quality -2 , 0)
+                if item.sell_in < 0:
+                    item.quality = max(item.quality -2 , 0)
+
+            else:
+                item.sell_in = item.sell_in - 1
+                if item.sell_in < 0:
+                    item.quality = max(item.quality - 1, 0)
+                item.quality = max(item.quality - 1, 0)
